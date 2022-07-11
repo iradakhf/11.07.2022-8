75f24e08f38cde686658dcdef4ff6d8c264518cf
iradakhf <iradakhf@code.edu.az>
2022-07-11 16:36:27 +0400

    add all

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ConsoleApp36
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Splitting splitting = new Splitting("hello friend");
+            string[] words = { };
+            for (int i = 0; i < splitting.Sentence.Length; i++)
+            {
+                words = splitting.ReturnSplittedWordsInArray(splitting.Sentence);
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+    class Splitting
+    {
+        public string Sentence { get; set; }
+
+        public Splitting(string sentence)
+        {
+            this.Sentence = sentence;
+
+        }
+        public string[] ReturnSplittedWordsInArray(string sentence)
+        { string[] words = sentence.Split(' ');
+           
+            return words;
+        }
+    }
+}
